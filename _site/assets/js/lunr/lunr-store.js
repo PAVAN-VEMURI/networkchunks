ea38cb453b124eed95ca38d7a995fc8615c6230d
PAVAN-VEMURI <vpavan318@gmail.com>
2021-12-10 17:53:57 +0000

    Added assets for icons, links to social media and author details.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Sample post",
-        "excerpt":" ","categories": [],
+        "title": "Lorem Ipsum",
+        "excerpt":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,...","categories": [],
         "tags": [],
         "url": "/2016/07/20/writing-jekyll-posts.html",
+        "teaser": null
+      },{
+        "title": "Another Lorem Ipsum",
+        "excerpt":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,...","categories": [],
+        "tags": [],
+        "url": "/2016/07/20/writing-jekyll-posts1.html",
         "teaser": null
       }]
