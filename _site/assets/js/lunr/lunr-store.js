52423eddbf2da0c3a63b2142e143591d517f8ad6
PAVAN-VEMURI <vpavan318@gmail.com>
2021-12-10 10:16:41 +0000

    Added _layouts folder taken from minimal-mistakes

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Sample post",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/2016/07/20/writing-jekyll-posts.html",
+        "teaser": null
+      }]
